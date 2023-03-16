298ae6e5ddb2aefe74e97e0a9ce643c1786903b5
yon24-0 <yonas.alebel23@gmail.com>
2023-03-16 11:21:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main a program that prints a line using printf
- * return 0
+ * main - a program that prints a line using printf
+ * return: 0 (success)
  */
 int main(void)
 {
