ca8db04df80af5eb81798a5750d0edb6c2a18384
yon24-0 <yonas.alebel23@gmail.com>
2023-03-16 12:41:43 -0700

    6-size.c

--- 6-size.c
+++ 6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long d;
 float f;
-printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long: %zu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
