d86acfa30e8e6a3fb26f1ca09fc82666478f6bc3
yon24-0 <yonas.alebel23@gmail.com>
2023-03-16 12:32:01 -0700

    6-size.c

--- 6-size.c
+++ 6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -a  prints the size of various types on the computer it is compiled and run on.
+ * main -a  prints the size of various types on the computer it is compiled
  * Return:0 (Success)
  */
 int main(void)
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long d;
 float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
