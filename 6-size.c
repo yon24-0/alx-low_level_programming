b68f043cf4018837cf30a5c7d73c9a508bae724c
yon24-0 <yonas.alebel23@gmail.com>
2023-03-16 12:35:30 -0700

    6-size.c

--- 6-size.c
+++ 6-size.c
@@ -10,7 +10,7 @@ int b;
 long int c;
 long long d;
 float f;
-printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long: %zu byte(s)\n", (unsigned long)sizeof(d));
