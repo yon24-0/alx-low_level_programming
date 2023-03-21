2fecbb8164dbca45e931d2e78f76a0d514e443b7
yon24-0 <yonas.alebel23@gmail.com>
2023-03-21 09:31:46 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the char to print 
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
