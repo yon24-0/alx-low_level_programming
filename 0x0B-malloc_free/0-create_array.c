ba614df5b88e6139b56819ab99232de43c6da80e
yon24-0 <yonas.alebel23@gmail.com>
2023-04-10 08:20:56 -0700

    0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
+ * Description: create array of size size and assign char c
  * Return: Nothing.
  */
 char *create_array(unsigned int size, char c)
