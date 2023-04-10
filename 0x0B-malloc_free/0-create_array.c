41f7b3ee16c924cbf91389127a3ae0138682049d
yon24-0 <yonas.alebel23@gmail.com>
2023-04-10 08:13:24 -0700

    0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
+       char *str;
+       unsigned int i;
 
-  if (size == 0)
-      return (NULL);
+       str = malloc(sizeof(char) * size);
+       if (size == 0 || str == NULL)
+	       return (NULL);
 
-  char *s = (char*)malloc(size * sizeof(char));
-
-  for (i = 0; i < size; i++)
-    s[i] = c;
-
-  return (s);
+       for (i = 0; i < size; i++)
+	       str[i] = c;
+       return (str);
 }
